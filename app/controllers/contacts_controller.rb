25f2f6f840e39090a29ca2c3c92135aa19ce9955
Di Su <disu.personal@gmail.com>
2019-10-13 09:09:30 +0000

    Added hirb gem

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,14 @@
 class ContactsController < ApplicationController 
   def new
+    # Executed on /contacts/new visit 
+    # Create a blank contact object {name: , email: , comments: }
     @contact = Contact.new
+    # Then pull the new.html.erb view (default)
   end
   
   def create
+    # Grab the form fields and assign them to the object
+    # {name: example, email: example, comments: example}
     @contact = Contact.new(contact_params)
     if @contact.save 
       redirect_to new_contact_path, notice: "Message sent."
@@ -12,8 +17,10 @@ class ContactsController < ApplicationController
     end
   end
   
+  # private = only to be used in this file
   private 
     def contact_params
+      # Save securely to the DB 
       params.require(:contact).permit(:name, :email, :comments)
     end
 end
